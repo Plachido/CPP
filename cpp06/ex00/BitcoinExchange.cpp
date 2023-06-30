e8b22179ccc053f5577156fc3abb164d4454f1c3
Placido Pellegriti <placidopellegriti@live.it>
2023-06-30 19:11:24 +0200

    Getting real

--- cpp06/ex00/BitcoinExchange.cpp
+++ cpp06/ex00/BitcoinExchange.cpp
@@ -9,9 +9,94 @@ BitcoinExchange::BitcoinExchange(std::string filename)
         std::cout << "Error: could not open file." << std::endl;
         return;
     }
-    this->csvParser();
+    parser(_input_file, _inputs, ",");
+    parser(_db_file, _rates, " | ");
 }
 
-void BitcoinExchange::csvParser(){};
+BitcoinExchange::~BitcoinExchange()
+{
+    _input_file.close();
+    _db_file.close();
+    _prices.clear();
+}
 
-BitcoinExchange::~BitcoinExchange(){};
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &src)
+{
+	*this = src;
+}
+
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other)
+{
+	this -> _db_file = other._db_file;
+    this -> _input_file = other._input_file;
+    this -> _prices = other._prices;
+	return *this;
+}
+
+
+bool BitcoinExchange::check_date(std::string date)
+{
+    if (date.length() != 10)
+        return false;
+    if (date[4] != '-' || date[7] != '-')
+        return false;
+    for (int i = 0; i < 10; i++)
+    {
+        if (i == 4 || i == 7)
+            continue;
+        if (!isdigit(date[i]))
+            return false;
+    }
+    std::string year = date.substr(0, 4);
+    std::string month = date.substr(5, 2);
+    std::string day = date.substr(8, 2);
+
+    if (std::stoi(year) <= 1900 || std::stoi(year) >= 2023)
+        return false;
+    if (std::stoi(month) < 1 || std::stoi(month) > 12)
+        return false;
+    if (std::stoi(day) < 1 || std::stoi(day) > 31)
+        return false;
+    return true;
+}
+
+
+
+void BitcoinExchange::parser(std::ifstream file, std::map<std::string, double> map, std::string delimitator)
+{
+    std::string date;
+    std::string n;
+    std::string line;
+    int i = 0;
+    
+    while (std::getline(file, line))
+        if (i++)
+        {
+            date = line.substr(0, line.find(delimitator));
+            date = date_conversion(date)
+            n = line.substr(line.find(delimitator) + 1, line.length());
+            map.insert(std::map<std::string, int>::value_type(date, n));
+        }
+
+
+}
+
+void convert()
+{
+    map<string, int>::iterator input_it;
+    map<string, int>::iterator rates_it;
+
+    for (input_it = _input.begin(); input_it != _input.end(); input_it++)
+    {
+        check_date(input_it->first));
+        check_value(input_it->second));
+        for (rates_it = _rates.begin(); rates_it != rates.end(); rates_it++)
+        {
+            while (rates_it->first < input_it->first)
+                ;
+            if (rates_it->first > input_it->first)
+                rates_it--;
+            std::cout << input_it->first << " => " << input_it->second*rates_it->second << std::endl;
+        }
+    }
+}
--- cpp06/ex00/BitcoinExchange.hpp
+++ cpp06/ex00/BitcoinExchange.hpp
@@ -9,13 +9,16 @@ class BitcoinExchange
     private:
         std::ifstream _db_file; //file with btc value over time
         std::ifstream _input_file; //file with dates and amount to convert
-        std::map<std::string, double> _dict; //map associating btc value to date
+        std::map<std::string, double> _rates; //map associating btc value to date
+        std::map<std::string, double> _inputs;    
     public:
+        void convert();
+        bool check_date(std::string);
         BitcoinExchange(std::string filename); //Constructor
         BitcoinExchange(const BitcoinExchange &src); //Copy constructor
         BitcoinExchange &operator=(const BitcoinExchange &other); //assignment operator
         double getExchangeRate(std::string, std::string); //two strings, date and amount (btc)
-        void csvParser(); //parser for the csv file
+        void parser(std::ifstream, std::map<std::string, double>, std::string); //generic parser called for both input and db
         ~BitcoinExchange(); //Destructor
 };
 
